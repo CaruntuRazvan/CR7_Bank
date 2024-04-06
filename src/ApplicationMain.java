97dbc47a023cc50a62c675a6a6e337fb63e8bcb9
Caruntu Razvan <115624498+CaruntuRazvan@users.noreply.github.com>
2024-04-06 21:11:12 +0300

    v1

--- src/ApplicationMain.java
+++ src/ApplicationMain.java
@@ -1,15 +1,63 @@
 public class ApplicationMain {
     public static void main(String[] args) {
         // Creăm un utilizator pentru testare
-        User user = new User("john_doe", "password123");
+        User user = new User("madalin_ciocan", "ciocanesku");
 
         // Simulăm procesul de autentificare
-        String enteredUsername = "john_doe";
-        String enteredPassword = "password123";
+        String enteredUsername = "madalin_ciocan";
+        String enteredPassword = "ciocanesku";
         if (user.authenticate(enteredUsername, enteredPassword)) {
             System.out.println("Autentificare reușită! Bun venit, " + user.getUsername() + "!");
         } else {
             System.out.println("Autentificare eșuată. Nume de utilizator sau parolă incorectă.");
         }
+        /*
+        System.out.println("Carduri asociate:");
+        for (Card card : user.getCards()) {
+            System.out.println("- Tip: " + card.getType());
+        }
+        */
+        Card standardCard = new Card(CardType.STANDARD);
+        PremiumCard premiumCard = new PremiumCard(CardType.PREMIUM);
+
+        user.addCard(standardCard);
+        user.addCard(premiumCard);
+
+        System.out.println("Carduri asociate:");
+        for (Card card : user.getCards()) {
+            System.out.println("- " + card.getType());
+        }
+
+        int chosenCardIndex = 0; // presupunem că utilizatorul a ales primul card din listă
+        Card chosenCard = user.interactWithCard(chosenCardIndex);
+        System.out.println("Ai ales să interacționezi cu cardul: " + chosenCard.getType());
+        // Afișarea informațiilor despre cardul ales
+        System.out.println(chosenCard.getCardInfo());
+        //Informatii dupa ce am adaugat si retras sume de bani
+        chosenCard.deposit(100);
+        chosenCard.withdraw(20);
+        System.out.println(chosenCard.getCardInfo());
+        //afisare istoric tranzactii
+        chosenCard.displayTransactions();
+
+        //transferam bani la cardul premium
+        chosenCard.transferMoney(premiumCard, 50);
+        System.out.println(chosenCard.getCardInfo());
+        int chosenCardIndex2 = 1; // presupunem că utilizatorul a ales primul card din listă
+
+        PremiumCard chosenCard2 = (PremiumCard) user.interactWithCard(chosenCardIndex2);
+        System.out.println(chosenCard2.getCardInfo());
+
+        chosenCard2.addToSavings(4);
+        System.out.println(chosenCard2.getSavingsBalance());
+        /*
+        System.out.println("Balanța RON : " + chosenCard2.getBalance() + " RON");
+        System.out.println("Balanța EUR : " + chosenCard2.getBalanceInEUR() + " EUR");
+
+        premiumCard.transferRONtoEUR(20);
+        System.out.println("Balanța RON dupa convertire: " + chosenCard2.getBalance() + " RON");
+        System.out.println("Balanța EUR dupa convertire: " + chosenCard2.getBalanceInEUR() + " EUR");
+        */
+
     }
 }
--- src/User.java
+++ src/User.java
@@ -1,20 +1,59 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
     private String username;
     private String password;
-
+    private List<Card> cards;
     // Constructor
     public User(String username, String password) {
         this.username = username;
-        this.password = password;
+        if(isValidPassword(password)) {
+            this.password = password;
+        } else{
+            throw new IllegalArgumentException("Parola trebuie să aiba minim 6 caractere!");
+        }
+        this.cards = new ArrayList<>();
     }
-
+    private boolean isValidPassword(String password) {
+        return password.length() >= 6;
+    }
     // Metoda pentru verificarea credențialelor de autentificare
     public boolean authenticate(String enteredUsername, String enteredPassword) {
         return username.equals(enteredUsername) && password.equals(enteredPassword);
     }
 
-    // Getter pentru numele utilizatorului
     public String getUsername() {
         return username;
     }
+    public List<Card> getCards() {
+        return cards;
+    }
+    public void addCard(Card card) {
+        CardType type = card.getType();
+        if (!hasCardOfType(type)) {
+            cards.add(card);
+        } else {
+            System.out.println("Utilizatorul deja deține un card de tipul " + type + ".");
+        }
+    }
+
+    private boolean hasCardOfType(CardType type) {
+        for (Card card : cards) {
+            if (card.getType() == type) {
+                return true; // Utilizatorul deține deja un card de acest tip
+            }
+        }
+        return false;
+    }
+
+    public void removeCard(Card card) {
+        cards.remove(card);
+    }
+
+    public Card interactWithCard(int index) {
+        return cards.get(index);
+
+    }
+
 }
